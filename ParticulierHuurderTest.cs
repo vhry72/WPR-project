fa6c4042de0971692b133bfd63a0020a902d28df
Byrott0 <amirsobhan2004@gmail.com>
2024-12-02 10:29:03 +0100

    test voor nu vw

---
+++ ParticulierHuurderTest.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace WPR_UnitTest
+{
+    public class ParticulierHuurderTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+
+
+            //act 
+
+            //assert
+
+        }
+    }
+}
