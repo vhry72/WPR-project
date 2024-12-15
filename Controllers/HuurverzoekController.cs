57a26494ede50a2be2d9d4423964ffad691f765a
vhry72 <102267967+vhry72@users.noreply.github.com>
2024-12-15 21:16:23 +0100

    get methodes waren verdwenen bij de merge

--- Controllers/HuurverzoekController.cs
+++ Controllers/HuurverzoekController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WPR_project.DTO_s;
 using WPR_project.Models;
 using WPR_project.Services;
 using WPR_project.Services.Email;
@@ -63,4 +64,59 @@ public class HuurverzoekController : ControllerBase
         }
     }
 
+
+    [HttpGet]
+    public IActionResult GetAllHuurVerZoeken()
+    {
+        try
+        {
+            var huurverzoeken = _service.GetAllHuurVerzoeken();
+            return Ok(huurverzoeken);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Interne serverfout: {ex.Message}");
+        }
+    }
+    // Haalt een specifiek Huurverzoek op d.m.v. huurderID
+    [HttpGet("{id}")]
+    public ActionResult<Huurverzoek> GetById(Guid id)
+    {
+        var huurder = _service.GetById(id);
+        if (huurder == null)
+        {
+            return NotFound(new { Message = "Huurder niet gevonden." });
+        }
+        return Ok(huurder);
+    }
+
+    // Werkt bij of een HuurVerzoek is GoedGekeurd of Afgekeurd.
+
+    [HttpPut("{id}")]
+    public IActionResult Update(Guid id, [FromBody] HuurVerzoekDTO dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            var errors = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
+            Console.WriteLine($"ModelState Errors: {string.Join(", ", errors)}");
+            return BadRequest(ModelState);
+        }
+        Console.WriteLine($"Route ID: {id}, DTO ID: {dto.HuurderID}");
+        if (id != dto.HuurderID)
+        {
+            return BadRequest(new { Message = "ID in de route komt niet overeen met het ID in de body." });
+        }
+        try
+        {
+            _service.Update(id, dto);
+            return NoContent();
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound(new { Message = "Huurverzoek niet gevonden." });
+        }
+    }
 }
+
+
+
