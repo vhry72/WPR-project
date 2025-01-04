dcbe99e58a4435e71a6c736d1bb9ff977cdac66e
Jay-jay Scholtes <22073841@student.hhs.nl>
2025-01-05 00:15:57 +0100

    .

---
+++ Views/src/pages/FrontOfficeMedewerker/VoertuigDetails.jsx
@@ -0,0 +1,44 @@
+﻿import React, { useEffect, useState } from 'react';
+
+
+const VoertuigenList = () => {
+    const [voertuigen, setVoertuigen] = useState([]);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        fetch('https://localhost:5033/api/Voertuigen')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Netwerk reactie was niet ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setVoertuigen(data); // Zet de opgehaalde data
+            })
+            .catch((error) => {
+                setError('Er is een fout opgetreden bij het ophalen van de Voertuigen.');
+            });
+    }, []);
+   
+    return (
+        <div>
+            <h1>Voertuigen</h1>
+            {error && <p>{error}</p>} {/* Toon een foutmelding als er een fout optreedt */}
+            {voertuigen.length === 0 ? (
+                <p>Geen voertuigen beschikbaar.</p>
+            ) : (
+                <ul>
+                    {voertuigen.map((voertuig) => (
+                        <li key={voertuig.voertuigId}> {/* Zorg ervoor dat de juiste ID wordt gebruikt */}
+                            <p>Voertuig: {voertuig.merk} {voertuig.model}</p>                            
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+};
+
+export default VoertuigenList;
