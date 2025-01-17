e5b3c3b659ef68787692c320be5e3abfe1d596b1
Veysel Yildirim <102267967+vhry72@users.noreply.github.com>
2025-01-17 19:37:49 +0100

    je kan nu inloggen door enter te klikken en 2fa is verbeterd doordat elke cijfer ny een apparte column heeft

--- Views/src/pages/Beveiliging/LoginVoorWijziging.jsx
+++ Views/src/pages/Beveiliging/LoginVoorWijziging.jsx
@@ -1,35 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import apiService from "../../services/apiService";
-import "../../styles/Login.css";
 import axios from "axios";
-import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import { toast } from 'react-toastify';
-
+import "../../styles/Login.css";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const { setUserRole } = useContext(UserContext);
     const [password, setPassword] = useState("");
-    const [twoFactorCode, setTwoFactorCode] = useState("");
+    const [twoFactorCode, setTwoFactorCode] = useState(Array(6).fill(""));
     const [userId, setUserId] = useState(null);
     const [errorMessage, setErrorMessage] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
-    const handleLogin = async () => {
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                if (!userId) { 
+                    handleLogin(event);
+                }
+            }
+        };
+
+        
+        window.addEventListener('keydown', handleKeyPress);
+
+        
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [email, password, userId]); 
+
+    const handleLogin = async (event) => {
         setIsLoading(true);
         setErrorMessage("");
-
         try {
-            const payload = {
-                email: email,
-                password: password,
-            };
-
-            const response = await apiService.post("/Account/login", { body: payload });
-
+            const payload = { email, password };
+            const response = await axios.post("https://localhost:5033/api/Account/login", payload);
             if (response.data && response.data.requiresTwoFactor) {
                 setUserId(response.data.userId);
             } else {
@@ -44,31 +54,23 @@ const Login = () => {
         }
     };
 
-
-    const handleVerifyTwoFactor = async () => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
         setIsLoading(true);
         setErrorMessage("");
-
         try {
-            const payload = {
-                userId: userId,
-                code: twoFactorCode,
-            };
-
+            const code = twoFactorCode.join('');
+            const payload = { userId, code };
             const response = await axios.post("https://localhost:5033/api/Account/verify-2fa", payload, {
-                withCredentials: true, // Ensures cookies are processed
+                withCredentials: true,
             });
 
             console.log("2FA successful:", response.data);
-            const userRole = response.data.role; // Get role from response
+            const userRole = response.data.role;
 
-            setUserRole(userRole); // Update de context
-            localStorage.setItem("role", userRole);; // Store role in localStorage for UI purposes
-
+            setUserRole(userRole);
+            localStorage.setItem("role", userRole);
             toast.success("Succesvol ingelogd!");
-            
-
-
             navigate("/");
         } catch (error) {
             console.error("2FA verification failed:", error.response || error);
@@ -78,46 +80,95 @@ const Login = () => {
         }
     };
 
+    const handleKeyDown = (index, event) => {
+        if (event.key === "Backspace" && twoFactorCode[index] === "") {
+            // Focus naar het vorige veld verplaatsen als het huidige veld leeg is en backspace wordt ingedrukt
+            const prevField = document.getElementById(`code-${index - 1}`);
+            if (prevField) {
+                prevField.focus();
+            }
+        }
+    };
+
+    const handleCodeChange = (index, event) => {
+        const value = event.target.value;
+        const newCode = [...twoFactorCode];
+
+        if (value === '' && twoFactorCode[index] !== '') {
+            // Verwijder de waarde als de gebruiker een backspace gebruikt in een niet-leeg veld
+            newCode[index] = '';
+            setTwoFactorCode(newCode);
+        } else if (value) {
+           
+            newCode[index] = value[0]; 
+            setTwoFactorCode(newCode);
+
+            if (index < 5) {
+                const nextField = document.getElementById(`code-${index + 1}`);
+                if (nextField) {
+                    nextField.focus();
+                }
+            }
+        }
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {  
+            event.preventDefault();  
+            handleLogin(event);
+        }
+    };
+
 
 
     return (
         <div className="login-container-wrapper">
             <div className="login-container">
                 {!userId ? (
-                    <div className="login-form">
+                    <form onSubmit={handleLogin} className="login-form">
                         <h2>Login</h2>
                         <input
-                            type="email"
-                            placeholder="E-mailadres"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="login-input"
+                        type="email"
+                        placeholder="E-mailadres"
+                        value={email}
+                        onInput={(e) => setEmail(e.target.value)}
+                        className="login-input"
+                        onKeyDown={handleKeyPress} // Voeg deze regel toe
                         />
                         <input
                             type="password"
                             placeholder="Wachtwoord"
                             value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            onInput={(e) => setPassword(e.target.value)}
                             className="login-input"
+                            onKeyDown={handleKeyPress} // Voeg deze regel toe
                         />
-                        <button onClick={handleLogin} disabled={isLoading} className="login-button">
+                        <button type="submit" disabled={isLoading}>
                             {isLoading ? "Inloggen..." : "Login"}
                         </button>
-                    </div>
+                    </form>
                 ) : (
-                    <div className="two-factor-form">
+                    <form onSubmit={handleSubmit} className="two-factor-form">
                         <h2>Tweestapsverificatie</h2>
-                        <input
-                            type="text"
-                            placeholder="Verificatiecode"
-                            value={twoFactorCode}
-                            onChange={(e) => setTwoFactorCode(e.target.value)}
-                            className="login-input"
-                        />
-                        <button onClick={handleVerifyTwoFactor} disabled={isLoading} className="login-button">
+                        <div className="code-input-container">
+                            {twoFactorCode.map((code, index) => (
+                                <input
+                                    key={index}
+                                    id={`code-${index}`}
+                                    type="text"
+                                    maxLength="1"
+                                    value={code}
+                                    onChange={(e) => handleCodeChange(index, e)}
+                                    onKeyDown={(e) => handleKeyDown(index, e)} 
+                                    className="code-input"
+                                    autoComplete="off"
+                                />
+                            ))}
+                        </div>
+                        <button type="submit" disabled={isLoading} className="login-button">
                             {isLoading ? "Verifiëren..." : "Verifieer"}
                         </button>
-                    </div>
+                    </form>
                 )}
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
             </div>
